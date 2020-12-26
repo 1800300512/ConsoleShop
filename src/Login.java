a5d7c795fcc75dfc04aa168224f3c6c888ab718e
Xinbaogu <969569231@qq.com>
2020-12-26 17:24:22 +0800

    验证xlse

--- src/Login.java
+++ src/Login.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Login {
@@ -10,5 +11,27 @@ public class Login {
         String passWord= sc.next();
         System.out.println("你输入的密码为：" + passWord);
         sc.close();
+        /*if(userName.equals("zhangsan")&&passWord.equals("123")){
+
+            System.out.println("登录成功");
+
+        }else{
+            System.out.println("登录失败");*/
+
+        File file =new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
+        ReadExcel read=new ReadExcel();
+        User user[]=read.readExcel(file);
+        boolean f=false;
+        for(int i =0;i<=user.length;i++){
+        if (user[i].getUsername().equals(userName)&&user[i].getPassword().equals(passWord)){
+            f=false;
+            break;
+        }
+        if(f){
+            System.out.println("登录成功");
+        }else {
+            System.out.println("登录失败");
+        }
+        }
     }
 }
