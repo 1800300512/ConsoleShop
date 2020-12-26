9dd998122ad3497ed7bb02951847dfc7c7173c43
Xinbaogu <969569231@qq.com>
2020-12-26 17:37:41 +0800

    验证xlse

--- src/Login.java
+++ src/Login.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] args) {
         System.out.println("请输入用户名");
-        Scanner sc=new Scanner(System.in);
-        String userName =sc.next();
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
         System.out.println("你输入的用户名为：" + userName);
         System.out.println("请输入密码");
-        String passWord= sc.next();
+        String passWord = sc.next();
         System.out.println("你输入的密码为：" + passWord);
         sc.close();
         /*if(userName.equals("zhangsan")&&passWord.equals("123")){
@@ -18,20 +18,22 @@ public class Login {
         }else{
             System.out.println("登录失败");*/
 
-        File file =new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
-        ReadExcel read=new ReadExcel();
-        User user[]=read.readExcel(file);
-        boolean f=false;
-        for(int i =0;i<=user.length;i++){
-        if (user[i].getUsername().equals(userName)&&user[i].getPassword().equals(passWord)){
-            f=false;
-            break;
+        File file = new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
+        ReadExcel read = new ReadExcel();
+        User user[] = read.readExcel(file);
+        boolean f = false;
+        for (int i = 0; i < user.length; i++) {
+            if (user[i].getUsername().equals(userName) && user[i].getPassword().equals(passWord)) {
+                f = true;
+                break;
+            }
+
         }
-        if(f){
+        if (f) {
             System.out.println("登录成功");
-        }else {
+        } else {
             System.out.println("登录失败");
         }
-        }
+
     }
 }
