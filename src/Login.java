7cf71ba47503a85cd29442732f10027dbd16e7d2
Xinbaogu <969569231@qq.com>
2020-12-27 15:16:41 +0800

    防蓝屏

--- src/Login.java
+++ src/Login.java
@@ -1,26 +1,23 @@
 import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Login {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("请输入用户名");
         Scanner sc = new Scanner(System.in);
         String userName = sc.next();
-        System.out.println("你输入的用户名为：" + userName);
+       //yzdlx6 System.out.println("你输入的用户名为：" + userName);
         System.out.println("请输入密码");
         String passWord = sc.next();
-        System.out.println("你输入的密码为：" + passWord);
+       // System.out.println("你输入的密码为：" + passWord);
         sc.close();
-        /*if(userName.equals("zhangsan")&&passWord.equals("123")){
 
-            System.out.println("登录成功");
 
-        }else{
-            System.out.println("登录失败");*/
-
-        File file = new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
+       // File file = new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
+        InputStream in = Class.forName("Login").getResourceAsStream("/1111.xlsx");
         ReadExcel read = new ReadExcel();
-        User user[] = read.readExcel(file);
+        User user[] = read.readExcel(in);
         boolean f = false;
         for (int i = 0; i < user.length; i++) {
             if (user[i].getUsername().equals(userName) && user[i].getPassword().equals(passWord)) {
