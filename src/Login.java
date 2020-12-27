2d21efd1ad496e15d18a442126e565c2f96c3bad
Xinbaogu <969569231@qq.com>
2020-12-27 15:35:35 +0800

    输入错误重新输入

--- src/Login.java
+++ src/Login.java
@@ -4,33 +4,37 @@ import java.util.Scanner;
 
 public class Login {
     public static void main(String[] args) throws ClassNotFoundException {
-        System.out.println("请输入用户名");
+        boolean f=true;
         Scanner sc = new Scanner(System.in);
-        String userName = sc.next();
-       //yzdlx6 System.out.println("你输入的用户名为：" + userName);
-        System.out.println("请输入密码");
-        String passWord = sc.next();
-       // System.out.println("你输入的密码为：" + passWord);
-        sc.close();
+        while(f) {
+            System.out.println("请输入用户名");
+
+            String userName = sc.next();
+            //yzdlx6 System.out.println("你输入的用户名为：" + userName);
+            System.out.println("请输入密码");
+            String passWord = sc.next();
+            // System.out.println("你输入的密码为：" + passWord);
 
 
-       // File file = new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
-        InputStream in = Class.forName("Login").getResourceAsStream("/1111.xlsx");
-        ReadExcel read = new ReadExcel();
-        User user[] = read.readExcel(in);
-        boolean f = false;
-        for (int i = 0; i < user.length; i++) {
-            if (user[i].getUsername().equals(userName) && user[i].getPassword().equals(passWord)) {
-                f = true;
-                break;
+
+            // File file = new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
+            InputStream in = Class.forName("Login").getResourceAsStream("/1111.xlsx");
+            ReadExcel read = new ReadExcel();
+            User user[] = read.readExcel(in);
+
+            for (int i = 0; i < user.length; i++) {
+                if (user[i].getUsername().equals(userName) && user[i].getPassword().equals(passWord)) {
+                    f = false;
+                    break;
+                }
+
             }
-
+            if (!f) {
+                System.out.println("登录成功");
+            } else {
+                System.out.println("登录失败");
+            }
         }
-        if (f) {
-            System.out.println("登录成功");
-        } else {
-            System.out.println("登录失败");
-        }
-
+        sc.close();
     }
 }
