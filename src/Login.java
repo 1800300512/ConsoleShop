a8bc3034b9253ab3117249e2a472ae53fcf80272
Xinbaogu <969569231@qq.com>
2020-12-27 17:00:47 +0800

    显示商品

--- src/Login.java
+++ src/Login.java
@@ -1,36 +1,40 @@
-import java.io.File;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class Login {
     public static void main(String[] args) throws ClassNotFoundException {
-        boolean f=true;
+        boolean f = true;
         Scanner sc = new Scanner(System.in);
-        while(f) {
+        while (f) {
             System.out.println("请输入用户名");
-
             String userName = sc.next();
-            //yzdlx6 System.out.println("你输入的用户名为：" + userName);
             System.out.println("请输入密码");
             String passWord = sc.next();
-            // System.out.println("你输入的密码为：" + passWord);
-
-
-
-            // File file = new File("C:\\Users\\lenovo\\Desktop\\111\\1111.xlsx");
             InputStream in = Class.forName("Login").getResourceAsStream("/1111.xlsx");
-            ReadExcel read = new ReadExcel();
+            InputStream out = Class.forName("Login").getResourceAsStream("/2222.xlsx");
+            ReadUserExcel read = new ReadUserExcel();
             User user[] = read.readExcel(in);
-
             for (int i = 0; i < user.length; i++) {
                 if (user[i].getUsername().equals(userName) && user[i].getPassword().equals(passWord)) {
                     f = false;
                     break;
                 }
-
             }
+
             if (!f) {
+                ReadProductExcel readProductExcel = new ReadProductExcel();
+                Product product[] = readProductExcel.readExcel(out);
                 System.out.println("登录成功");
+                System.out.print("商品id" + "\t");
+                System.out.print("商品名字" + "\t");
+                System.out.print("商品价格" + "\t");
+                System.out.println("商品描述");
+                for (int i = 0; i < product.length; i++) {
+                    System.out.print(product[i].getId() + "\t");
+                    System.out.print(product[i].getName() + "\t");
+                    System.out.print(product[i].getPrice() + "\t");
+                    System.out.println(product[i].getDsc());
+                }
             } else {
                 System.out.println("登录失败");
             }
---
+++ src/Product.java
@@ -0,0 +1,38 @@
+public class Product {
+    private  String id;
+    private String name;
+    private  float price;
+    private  String dsc;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDsc() {
+        return dsc;
+    }
+
+    public void setDsc(String dsc) {
+        this.dsc = dsc;
+    }
+}
