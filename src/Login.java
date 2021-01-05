a5aa566abb050c813fb0a69be9c78a8b2d8a6098
Xinbaogu <969569231@qq.com>
2021-01-05 20:06:44 +0800

    购物

--- src/Login.java
+++ src/Login.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] args) throws ClassNotFoundException {
         boolean f = true;
-        boolean t=false;
+        boolean t = false;
         Scanner sc = new Scanner(System.in);
         while (f) {
             System.out.println("请输入用户名");
@@ -28,13 +28,14 @@ public class Login {
                 for (int i = 0; i < product.length; i++) {
                     System.out.print(product[i].getId() + "\t");
                     System.out.print(product[i].getName() + "\t");
-                    if(i!=0) {
+                    if (i != 0) {
                         System.out.print(product[i].getPrice() + "\t");
-                    }else
+                    } else
                         System.out.print("price" + "\t");
                     System.out.println(product[i].getDsc());
-                }int k=0;
-                int l=0;
+                }
+                int k = 0;
+                int l = 0;
                 Product spc[] = new Product[10];
                 while (true) {
                     System.out.println("请输入要购买的商品编号");
@@ -52,25 +53,28 @@ public class Login {
                     if (!t) {
                         System.out.println("没有这个商品");
                     }
-                    if(l==0)
-                    {
+                    if (l == 0) {
                         System.out.println("是否继续购买，继续输入0，不继续输入1");
-                        l=sc.nextInt();
-                        if(l==1)
+                        l = sc.nextInt();
+                        if (l == 1)
                             break;
                     }
                 }
-                float a=0;
-                for(int j=0;j<spc.length;j++)
-                {
-                    System.out.println(spc[j].getId());
-                    System.out.println(spc[j].getName());
-                    System.out.println(spc[j].getPrice());
-                    System.out.println(spc[j].getDsc());
-                    a=a+spc[j].getPrice();
+                float a = 0;
+                try {
+                    for (int j = 0; j < spc.length; j++) {
+                        if (spc[j].getId() != null) {
+                            System.out.println(spc[j].getId());
+                            System.out.println(spc[j].getName());
+                            System.out.println(spc[j].getPrice());
+                            System.out.println(spc[j].getDsc());
+                            a = a + spc[j].getPrice();
+                        }
+                    }
+                    System.out.println("总价格：" + a);
+                } catch (NullPointerException e) {
 
                 }
-                System.out.println("总价格："+a);
             } else {
                 System.out.println("登录失败");
             }
